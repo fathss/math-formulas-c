1fbb57c23695ced1dda0971520628060cbae899d
fathss <alwifathu@gmail.com>
2024-12-12 20:25:41 +0700

    Update factorial.c

--- Factorial/factorial.c
+++ Factorial/factorial.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
-typedef long long int llint;
-llint factorial(llint);
+typedef unsigned long long ullint;
+ullint factorial(llint);
 
 int main(){
 
     int n;
     puts("A program for generating a factorial of a non-negative integer n, denoted by n!");
-    puts("However, due to the limitations of data types in C, this program can only represent 0 <= n <= 20");
+    puts("Due to the limitations of data types in C, this program can only represent 0 <= n <= 20 factorial");
     printf(">>>> ");
     scanf("%d", &n);
-    printf(">>>> %lld", factorial(n));
+    printf(">>>> %llu", factorial(n));
 
     return 0;
 }
 
-llint factorial(llint baseNumber){
+ullint factorial(ullint baseNumber){
     if (baseNumber == 0 || baseNumber == 1){
         return 1;
     }
