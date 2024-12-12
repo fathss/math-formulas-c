139edd6884b3c6c3f281ad4f80240360a9c81a1e
fathss <alwifathu@gmail.com>
2024-12-12 19:22:12 +0700

    Update factorial.c

--- Factorial/factorial.c
+++ Factorial/factorial.c
@@ -5,20 +5,20 @@ llint factorial(llint);
 
 int main(){
 
-    int number;
+    int n;
     puts("A program for generating a factorial of a non-negative integer n, denoted by n!");
     puts("However, due to the limitations of data types in C, this program can only represent 0 <= n <= 20");
     puts("Input the factorial number");
     printf(">>>> ");
-    scanf("%d", &number);
-    printf(">>>> %lld", factorial(number));
+    scanf("%d", &n);
+    printf(">>>> %lld", factorial(n));
 
     return 0;
 }
 
-llint factorial(llint x){
-    if (x == 0 || x == 1){
+llint factorial(llint baseNumber){
+    if (baseNumber == 0 || baseNumber == 1){
         return 1;
     }
-    return x * factorial(x-1);
+    return baseNumber * factorial(baseNumber-1);
 }
