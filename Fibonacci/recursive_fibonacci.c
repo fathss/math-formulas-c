bb689d3055e2751ddccfe213e4cac75b8ccda645
fathss <alwifathu@gmail.com>
2024-12-12 20:09:06 +0700

    Create recursive_fibonacci.c

---
+++ Fibonacci/recursive_fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int fibonacci(int);
+
+int main(){
+
+    int n;
+    puts("A program for generating a fibonacci sequence up to given number\n");
+    printf(">>>> ");
+    scanf("%d", &n);
+
+    if (n > 46){
+        puts("Due to repeated calculation in recursive function,\nto avoid huge time complexity, this program only limits the input until the 46th sequence.\n");
+        puts("Consider using the iterative solution for a sequence that exceeds 46th sequence");
+        exit(1);
+    }
+    printf(">>>> ");
+
+    for (int i = 0; i <= n; i++){
+        printf("%d ", fibonacci(i));
+    }
+    return 0;
+}
+
+int fibonacci(int num_terms){
+    if (num_terms == 0){
+        return 0;
+    } else if (num_terms == 1 || num_terms == 2){
+        return 1;
+    } else{
+        return fibonacci(num_terms - 1) + fibonacci(num_terms - 2);
+    }
+}
